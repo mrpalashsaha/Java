9fde9a2503ec56d47ea1e4fa23a81dc0fd4d65bb
Palash Saha <61386800+mrpalashsaha@users.noreply.github.com>
2020-06-13 10:13:06 +0300

    Create Sring.java

---
+++ Sring.java
@@ -0,0 +1,16 @@
+// string is immutable so it can not be changed by itsef what it does just take a copy of it and convert it ti upper case or lower case 
+character (static method); {
+string s1 = "Hello";
+strngh s2 = " World" ;
+string s3 = s1 + s2;
+} 
+return 0;
+
+if ( s1.euals(str)){
+str.substring (1,3);
+}
+for ( iint i = 0; i < str.length (); i ++) {
+char ch = str.charAt(i);
+
+}
+return 0; 
