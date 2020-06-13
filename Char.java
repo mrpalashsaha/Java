1e9e931f4010022fd675bcb7520b8e86561f7572
Palash Saha <61386800+mrpalashsaha@users.noreply.github.com>
2020-06-13 10:03:59 +0300

    Create Char.java

---
+++ Char.java
@@ -0,0 +1,13 @@
+// convert the char to lower case 
+// char should not be in the double " quite if it is in the " " it represent as string
+ 
+ public char toLower (char ch) {
+ if (ch >= 'A' ) && ch <= 'Z') {
+ return ((ch - 'A') + 'a'); 
+ }
+ return ch;
+ }
+ for char ch = 'A';
+ ch <= 'Z';
+ ch ++;
+ }
